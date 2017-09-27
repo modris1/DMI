cc9d6e8fc47b2ea675320bed65b4e1a4f727b459
saulespuke101@gmail.com <modris.upen@gmail.com>
2017-09-27 11:47:17 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-NAME="Modris Upenieks"
-echo $NAME
 
-#readonly NAME
+#4.piemērs
+echo "Tiek pildīts skripts: "$0
+#echo $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: "$#
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 1): "$*
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 2): "$@
+#echo $?
+echo "Skriptam piešķirtais procesa numrus: "$$
+echo ""$!
+
+#3.piemērs
+#NAME="Modris Upenieks"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
+#2.piemērs
+#NAME="Modris Upenieks"
+#readonly NAME
+#echo $NAME
 #NAME="Upenieks Modris"
 #echo $NAME
 
-#echo $NAME
-#unset NAME
+#1.piemērs
+#NAME="Modris Upenieks"
 #echo $NAME
